044a22ec74fca8048de29023d5947828343cb5e5
EvaHolden <eva.e.holden@gmail.com>
2023-08-11 01:57:55 +1000

    Added pdf reading functionality

--- testing.py
+++ testing.py
@@ -1,6 +1,7 @@
 from sentence_transformers import SentenceTransformer, util
 import requests
 import json
+import PyPDF2
 
 # https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
 # https://huggingface.co/tasks/sentence-similarity
@@ -22,3 +23,52 @@ data = query(
         }
     })
 
+## data = [0.050691038370132446, 0.6501005291938782, 0.7971435189247131]
+
+
+## Extract text from source article
+def extract_from_pdf(file_name):
+    """"""
+    # creating a pdf file object
+    pdfFileObj = open(file_name, 'rb')
+
+    pdf_text = ""
+    
+    # creating a pdf reader object
+    pdfReader = PyPDF2.PdfReader(pdfFileObj)
+    
+    for i in range(len(pdfReader.pages)):
+    
+        # creating a page object
+        pageObj = pdfReader.pages[i]
+        
+        # extracting text from page
+        pdf_text = pdf_text + pageObj.extract_text()
+    
+    # closing the pdf file object
+    pdfFileObj.close()
+
+    return pdf_text
+
+liquidBiopsyArticle = extract_from_pdf('name-tbd\liquidBiopsyArticle.pdf')
+liquidBiopsyPaper = extract_from_pdf('name-tbd\liquidBiopsy.pdf')
+openDayArticle = extract_from_pdf('name-tbd\openDayArticle.pdf')
+
+data = query(
+    {
+        "inputs": {
+            "source_sentence": liquidBiopsyPaper,
+            "sentences":[liquidBiopsyArticle,
+                         openDayArticle]
+        }
+    })
+
+## data = [0.4563927948474884, 0.026159482076764107]
+# despite the tragic formatting it's still getting a significantly higher similarity score for the relevant article
+# obviously more testing would be needed, in particular for adjacent areas of science
+# Could potentially be used 'as is' to provide a confidence score for whether an article is based in truth,
+# but the real value would come from being to identify what in the article is accurate and what isn't
+
+# break the individual blocks of text into more valuable individual sentences
+# perform semantic similarity on all pairs in source and reference
+# scores greater than x are considered a good match and likely indicate accuracy, lack of indicates inaccurate information
